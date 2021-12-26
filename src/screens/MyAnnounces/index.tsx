2980a634ad039075ee04233780e768a64a885149
PGP27 <peg.gep27@gmail.com>
2021-12-26 17:11:56 -0300

    Latest update

--- src/screens/MyAnnounces/index.tsx
+++ src/screens/MyAnnounces/index.tsx
@@ -8,51 +8,57 @@ import Header from '../../components/Header';
 import NavigationBar from '../../components/NavigationBar';
 
 const MyAnnounces = () => {
-  // const [myAnnounces, setMyAnnounces] = useState<any>();
-  // const [userId, setUserId] = useState<any>();
-  // const [pictures, setPictures] = useState([]);
-  // const { user } = useAuth();
+  const [myAnnounces, setMyAnnounces] = useState<any>();
+  const [userId, setUserId] = useState<any>();
+  const [pictures, setPictures] = useState([]);
+  const { user } = useAuth();
 
-  // useEffect(() => {
-  //   const init = async () => {
-  //     const getAllUsers = await database.collection('users').get();
-  //     const allUsers = getAllUsers.docs.map((doc) => doc.data());
-  //     const currentUser = allUsers.find(({ email }) => email === user.email);
-  //     const getAllAnnounces = await database.collection('announces').get();
-  //     const allAnnounces = getAllAnnounces.docs.map((doc) => doc.data());
-  //     const announces = allAnnounces.filter(({ advertiserId }) => advertiserId === currentUser.id);
-  //     setMyAnnounces(announces);
-  //     setUserId(currentUser.id);
-  //   };
-  //   init();
-  // }, []);
+  useEffect(() => {
+    const init = async () => {
+      const getAllUsers = await database.collection('users').get();
+      const allUsers = getAllUsers.docs.map((doc) => doc.data());
+      const currentUser = allUsers.find(({ email }) => email === user.email);
+      const getAllAnnounces = await database.collection('announces').get();
+      const allAnnounces = getAllAnnounces.docs.map((doc) => doc.data());
+      const announces = allAnnounces.filter(({ advertiserId }) => advertiserId === currentUser.id);
+      setMyAnnounces(announces);
+      setUserId(currentUser.id);
+    };
+    init();
+  }, []);
 
-  // const downloadImageById = async (id: string) => {
-  //   const response = await storage.ref(`announces/${userId}`).child(id).getDownloadURL();
-  //   const uri = await response;
-  //   setPictures((state) => [...state, uri]);
-  // };
+  const downloadImageById = async (id: string) => {
+    const response = await storage.ref(`announces/${userId}`).child(id).getDownloadURL();
+    const uri = await response;
+    setPictures((state) => [...state, uri]);
+    console.log('olá');
+  };
 
-  return (
-    <Container>
-      <Header />
-      <Title>Meus anúncios</Title>
-      <ScrollView>
-        {/* {userId && myAnnounces.map(({ title, mainPictureId }, index) => {
-          downloadImageById(mainPictureId);
-          return (
-            <AnnounceSimpleCard
-              key={index}
-              title={title}
-              image={pictures[index]}
-              iconName="delete"
-            />
-          );
-        })} */}
-      </ScrollView>
-      <NavigationBar />
-    </Container>
-  );
+  if (userId) {
+    return (
+      <Container>
+        <Header />
+        <Title>Meus anúncios</Title>
+        <ScrollView>
+          {myAnnounces.map(({ title, mainPictureId }, index) => {
+            console.log('olá');
+            // downloadImageById(mainPictureId);
+            return (
+              <AnnounceSimpleCard
+                key={index}
+                title={title}
+                image={pictures[index]}
+                iconName="delete"
+              />
+            );
+          })}
+        </ScrollView>
+        <NavigationBar />
+      </Container>
+    );
+  }
+
+  return null;
 };
 
 export default MyAnnounces;
