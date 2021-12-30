59fb3cb67504bb40dedb996513be342a308be19b
Pedro <pedro@MacBook-Pro-de-Pedro.local>
2021-12-30 11:55:59 -0300

    Latest update

--- src/screens/Search/index.tsx
+++ src/screens/Search/index.tsx
@@ -1,26 +1,44 @@
-import React, { useEffect, useState } from 'react';
-import { Container, Header, Input, Text, SearchOptions, OptionButton, OptionText } from './styles';
+import React, { useState } from 'react';
+import { database } from '../../../firebase';
 import { StatusBar } from 'react-native';
 import NavigationBar from '../../components/NavigationBar';
+import { useAuth } from '../../contexts/AuthContext';
+import { Container, Header, Input, Text, SearchOptions, OptionButton, OptionText } from './styles';
 
-const Search = ({ setOpenSearchModal }) => {
-  const [selectedOption, setSelectedOption] = useState('Endereço');
+const Search = () => {
+  const [inputText, setInputText] = useState('');
+  const [selectedOption, setSelectedOption] = useState('address');
+  const { user } = useAuth();
+
+  const searchAnnounce = async () => {
+    const getAllUsers = await database.collection('announces').get();
+    const allUsers = getAllUsers.docs.map((doc) => doc.data());
+    const currentUser = allUsers.find(({ email }) => email === user.email);
+
+    const getAllAnnounces = await database.collection('announces').get();
+    const allAnnounces = getAllAnnounces.docs.map((doc) => doc.data());
+    const otherAnnounces = allAnnounces.filter(({ advertiserId }) => advertiserId !== currentUser.id);
+  };
 
   return (
     <Container>
+      <StatusBar backgroundColor="white" barStyle="dark-content" />
       <Header>
-        <Input placeholder="Pesquise aqui..." underlineColorAndroid="gray" />
+        <Input value={inputText} onChangeText={(text) => setInputText(text)} onSubmitEditing={searchAnnounce} returnKeyType="search" placeholder="Pesquise aqui..." underlineColorAndroid="gray" />
       </Header>
       <Text>Buscar por:</Text>
       <SearchOptions>
-        <OptionButton option="Endereço" onPress={() => setSelectedOption('Endereço')} selected={selectedOption === 'Endereço'}>
-          <OptionText selected={selectedOption === 'Endereço'}>Endereço</OptionText>
+        <OptionButton option="address" onPress={() => setSelectedOption('address')} selected={selectedOption === 'address'}>
+          <OptionText selected={selectedOption === 'address'}>Endereço</OptionText>
         </OptionButton>
-        <OptionButton option="Bairro" onPress={() => setSelectedOption('Bairro')} selected={selectedOption === 'Bairro'}>
-          <OptionText selected={selectedOption === 'Bairro'}>Bairro</OptionText>
+        <OptionButton option="city" onPress={() => setSelectedOption('city')} selected={selectedOption === 'city'}>
+          <OptionText selected={selectedOption === 'city'}>Cidade</OptionText>
         </OptionButton>
-        <OptionButton option="Cidade" onPress={() => setSelectedOption('Cidade')} selected={selectedOption === 'Cidade'}>
-          <OptionText selected={selectedOption === 'Cidade'}>Cidade</OptionText>
+        <OptionButton option="state" onPress={() => setSelectedOption('state')} selected={selectedOption === 'state'}>
+          <OptionText selected={selectedOption === 'state'}>Estado</OptionText>
+        </OptionButton>
+        <OptionButton option="price" onPress={() => setSelectedOption('price')} selected={selectedOption === 'price'}>
+          <OptionText selected={selectedOption === 'price'}>Preço</OptionText>
         </OptionButton>
       </SearchOptions>
       <NavigationBar selected="Search" />
