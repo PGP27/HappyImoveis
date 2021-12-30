9c236b8608014613d8db7aacc19196f3c8467f76
PGP27 <peg.gep27@gmail.com>
2021-12-30 20:02:14 -0300

    Latest update

--- src/screens/Search/index.tsx
+++ src/screens/Search/index.tsx
@@ -4,20 +4,36 @@ import { StatusBar } from 'react-native';
 import NavigationBar from '../../components/NavigationBar';
 import { useAuth } from '../../contexts/AuthContext';
 import { Container, Header, Input, Text, SearchOptions, OptionButton, OptionText } from './styles';
+import { useNavigation } from '@react-navigation/native';
 
 const Search = () => {
   const [inputText, setInputText] = useState('');
   const [selectedOption, setSelectedOption] = useState('address');
   const { user } = useAuth();
+  const navigation =  useNavigation();
 
   const searchAnnounce = async () => {
-    const getAllUsers = await database.collection('announces').get();
+    const getAllUsers = await database.collection('users').get();
     const allUsers = getAllUsers.docs.map((doc) => doc.data());
     const currentUser = allUsers.find(({ email }) => email === user.email);
-
+    
     const getAllAnnounces = await database.collection('announces').get();
     const allAnnounces = getAllAnnounces.docs.map((doc) => doc.data());
     const otherAnnounces = allAnnounces.filter(({ advertiserId }) => advertiserId !== currentUser.id);
+    const filteredAnnounces = otherAnnounces.filter((announce) => announce[`${selectedOption}`].toLowerCase().includes(inputText.toLowerCase()));
+
+    let pageName = '';
+    if (selectedOption === 'address') pageName = 'endereço';
+    if (selectedOption === 'city') pageName = 'cidade';
+    if (selectedOption === 'state') pageName = 'estado';
+
+    navigation.navigate(
+      'ShowAnnounces',
+      {
+        pageName: `Buscando por ${pageName}: "${inputText}"`,
+        announces: filteredAnnounces,
+      }
+    )
   };
 
   return (
@@ -36,9 +52,6 @@ const Search = () => {
         </OptionButton>
         <OptionButton option="state" onPress={() => setSelectedOption('state')} selected={selectedOption === 'state'}>
           <OptionText selected={selectedOption === 'state'}>Estado</OptionText>
-        </OptionButton>
-        <OptionButton option="price" onPress={() => setSelectedOption('price')} selected={selectedOption === 'price'}>
-          <OptionText selected={selectedOption === 'price'}>Preço</OptionText>
         </OptionButton>
       </SearchOptions>
       <NavigationBar selected="Search" />
